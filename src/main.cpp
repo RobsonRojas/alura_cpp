d1067cb1cb0ed8125bb875ffb864e43c153a9717
rrandrade <rrandrade@blockbit.com>
2023-01-30 15:50:58 -0300

    First

---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main(int argc, char *argv[])
+{
+	std::cout << "***************************************" << std::endl;
+	std::cout << "*  Bem vindo ao jogo de advinhação! *" << std::endl;
+	std::cout << "***************************************" << std::endl;
+
+	std::cout << "Escolha o seu nível de dificuldade: " << std::endl;
+	std::cout << "Fácil (F), Médio (M) ou Difícil (C)" << std::endl;
+	char dificuldade;
+	std::cin >> dificuldade;
+
+	int numero_tentativas = 0;
+
+	if (dificuldade == 'F') {
+		numero_tentativas = 15;
+	} else if (dificuldade == 'M') {
+		numero_tentativas = 10;
+	} else if (dificuldade == 'C') {
+		numero_tentativas = 5;
+	} else {
+		numero_tentativas = 5;
+	}
+
+	srand(time(NULL));
+	const int NUMERO_SECRETO = rand() % 100 + 1;
+
+	// std::cout << "NUMERO SECRETO: " << NUMERO_SECRETO << std::endl;
+
+	int chute = 0;
+	int tentativas = 0;
+	double pontos = 1000.0;
+	bool acertou = false;
+
+	for (tentativas = 1; tentativas <= numero_tentativas; tentativas++) {
+		std::cout << "Dê o chute: " << std::endl;
+		
+		std::cin >> chute;
+		std::cout << "Chute: " << chute << std::endl;
+		double pontos_perdidos = std::abs((chute - NUMERO_SECRETO) / 2.0);
+		pontos -= pontos_perdidos;
+		if (chute == NUMERO_SECRETO) {
+			std::cout << "Parabéns! Você acertou o número secreto!" << std::endl;
+			acertou = true;
+			break;
+		} else if (chute > NUMERO_SECRETO) {
+			std::cout << "Seu chute foi maior que o número secreto!" << std::endl;	
+		} else {
+			std::cout << "Seu chute foi menor que o número secreto!" << std::endl;
+		}
+	}
+
+	std::cout << "Fim de jogo!" << std::endl;
+	if (acertou == false) {
+		std::cout << "Você perdeu! Tente novamente." << std::endl;
+	} else {
+		std::cout << "Você acertou em " << tentativas << " tentativas!" << std::endl;
+		std::cout.precision(2);
+		std::cout << std::fixed;
+		std::cout << "Sua pontuação foi de: " << pontos << std::endl;		
+	}
+
+	return 0;
+}
